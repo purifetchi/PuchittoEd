d4c0093530f94481d77e6ed95063e3434229cd0a
purifetchi <0xlunaric@gmail.com>
2026-04-14 19:16:00 +0200

    feat: better camera movement.

--- src/renderer/src/editor/entities/editorCameraObject.ts
+++ src/renderer/src/editor/entities/editorCameraObject.ts
@@ -2,14 +2,16 @@ import type { CameraEntityData } from 'puchitto/level'
 import { CameraObject, type GameObjectOptions } from 'puchitto/objects'
 import { Vector3 } from 'three'
 
+const WORLD_Y_AXIS = new Vector3(0, 1, 0)
+
 export class EditorCameraObject extends CameraObject {
   constructor(opts: GameObjectOptions & CameraEntityData) {
     super(opts)
+    this.tag = 'editor'
   }
 
   /**
    * Updates the editor camera's movement.
-   * @param dt The delta.
    */
   tick(dt: number): void {
     if (!this.game.input.mouseHeld) {
@@ -17,10 +19,24 @@ export class EditorCameraObject extends CameraObject {
     }
 
     // Rotate the camera based on the delta!
-    const yAxis = new Vector3(0, 1, 0)
-    const xAxis = new Vector3(1, 0, 0)
-    const delta = this.game.input.mouseDelta
-    this.threeObject.rotateOnWorldAxis(xAxis, delta.y * dt * 0.1)
-    this.threeObject.rotateOnWorldAxis(yAxis, delta.x * dt * 0.1)
+    const delta = this.game.input.mouseDelta.divide(this.game._getResolution())
+    this.threeObject.rotateX(delta.y)
+    this.threeObject.rotateOnWorldAxis(WORLD_Y_AXIS, delta.x)
+
+    if (this.game.input.keyDown('KeyW')) {
+      this.threeObject.translateZ(-7 * dt)
+    }
+
+    if (this.game.input.keyDown('KeyS')) {
+      this.threeObject.translateZ(7 * dt)
+    }
+
+    if (this.game.input.keyDown('KeyA')) {
+      this.threeObject.translateX(-7 * dt)
+    }
+
+    if (this.game.input.keyDown('KeyD')) {
+      this.threeObject.translateX(7 * dt)
+    }
   }
 }
