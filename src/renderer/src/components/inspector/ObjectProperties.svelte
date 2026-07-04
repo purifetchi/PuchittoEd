41655b57e9d6cdf2cea67d84bd52bc5fd27bb113
purifetchi <0xlunaric@gmail.com>
2026-07-04 20:07:19 +0200

    feat: add type info to object properties.

--- src/renderer/src/components/inspector/ObjectProperties.svelte
+++ src/renderer/src/components/inspector/ObjectProperties.svelte
@@ -5,11 +5,17 @@
   import InspectorRow from './InspectorRow.svelte'
   import Checkbox from '../common/Checkbox.svelte'
   import Input from '../common/Input.svelte'
+  import { editor } from '../../editor/editorGame'
+  import { PlaceholderObject } from '../../editor/entities/placeholderObject'
 
   let { obj }: { obj: GameObject } = $props()
 
   let name = $state('')
   let visible = $state(true)
+
+  const type = $derived(
+    obj instanceof PlaceholderObject ? obj.type : editor._entityFactory.resolveType(obj)
+  )
 
   $effect(() => {
     name = obj.name
@@ -34,8 +40,15 @@
     <Checkbox bind:checked={visible} />
     <Box />
     <Input bind:value={name} />
+    <div class="type">
+      {type}
+    </div>
   </InspectorRow>
 </InspectorPart>
 
 <style>
+  .type {
+    color: var(--text-muted);
+    font-family: var(--font-mono);
+  }
 </style>
